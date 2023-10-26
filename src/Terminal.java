e83d8386d0cd9df1ed081102a0e3110914078369
joue-zero <elnagaryoussef864@gmail.com>
2023-10-26 12:58:33 +0300

    The Structure Of The Files => Youssef

---
+++ src/Terminal.java
@@ -0,0 +1,39 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Terminal {
+    Parser parser;
+    String currentPath = null;
+
+    // Add Commands Here
+    public void echo(String input){
+
+    }
+    public String pwd(){
+
+    }
+    public void cd(String[] args){
+
+    }
+    public void ls(String options){
+
+    }
+
+    public void mkdir(String[] args){
+
+    }
+    public void rmdir(String[] args){
+
+    }
+
+    public void touch(String[] args){
+
+    }
+    // ...
+    //This method will choose the suitable command method to be called
+    public void chooseCommandAction(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
